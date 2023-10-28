b261ca9490e82e86fb339fc49498064cac02f072
Ano Rangga <rahardikaku@gmail.com>
2023-10-28 16:32:23 +0700

    first init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: Ano Rangga Rahardika <rahardikaku@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+module.exports = {
+	globals: {
+		appVersion: true
+	},
+	parserOptions: {
+		requireConfigFile: false
+	},
+	extends: [
+		'@nextcloud'
+	],
+	rules: {
+		'jsdoc/require-jsdoc': 'off',
+		'jsdoc/tag-lines': 'off',
+		'vue/first-attribute-linebreak': 'off',
+		'import/extensions': 'off'
+	}
+}
